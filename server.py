9035fe00f3b454b43c6e19f874c095be04dc8a81
Pratham S <snehi.pratham@gmail.com>
2024-09-16 08:01:00 -0500

    create flask app

--- server.py
+++ server.py
@@ -1,4 +1,15 @@
-from flask import Flask
+"""Server side for the template of the portfolio website"""
+from flask import Flask, abort, render_template, redirect, url_for, flash, request
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "hururururuurur"
 
+@app.route("/", methods=["POST", "GET"])
+def homepage():
+    """Homepage for the portfolio website"""
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
