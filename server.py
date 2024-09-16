1fe847b760d4395270dc06fc092ef8263f29e08b
Pratham S <snehi.pratham@gmail.com>
2024-09-16 08:14:21 -0500

    add environment variable

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 """Server side for the template of the portfolio website"""
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
-
+import os
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "hururururuurur"
+app.config["SECRET_KEY"] = os.environ.get("app_key")
 
 @app.route("/", methods=["POST", "GET"])
 def homepage():
