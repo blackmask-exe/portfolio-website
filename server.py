3ffa133b4f0aacbbd9b16ec790dea99e641321c9
Pratham S <snehi.pratham@gmail.com>
2024-09-16 08:41:37 -0500

    add gunicorn config file

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 """Server side for the template of the portfolio website"""
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 import os
+import gunicorn
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("app_key")
 
